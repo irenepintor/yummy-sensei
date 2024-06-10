fc1c1d197e96714545e63cba651825771475a3fe
Irene Pintor <103829940+irenepintor@users.noreply.github.com>
2024-06-10 08:31:48 -0300

    Update responsive design ItemDetail.jsx

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -14,23 +14,23 @@ const ItemDetail = ({ product }) => {
     return (
         <>
             <ProductCategories/>
-            <div className="max-w-4xl mx-auto p-6 mt-5 mb-4 bg-white shadow-md rounded-lg flex">
-                <div className="w-1/2">
-                    <img className="w-full h-auto object-cover rounded-lg" src={product.image} alt={product.name} />
-                </div>
-                <div className="w-1/2 pl-10">
-                    <h1 className="text-2xl font-bold text-gray-900 mb-2">{product.name}</h1>
-                    <div className="text-xl font-semibold text-gray-700 mb-4">${product.price}</div>
-                    <p className="text-gray-700 mb-6">{product.description}</p>
-                    <div className="flex mb-4">
-                        <ItemCount 
-                            stock={product.stock}
-                            initial={1}
-                            onAdd={onAdd} />
-                    </div>
-                </div>
+            <div className="max-w-4xl mx-auto p-6 mt-5 mb-4 bg-white shadow-md rounded-lg flex flex-col md:flex-row">
+                 <div className="w-full md:w-1/2">
+                      <img className="w-full h-auto object-cover rounded-lg" src={product.image} alt={product.name} />
+                 </div>
+                 <div className="w-full md:w-1/2 mt-4 md:mt-0 md:pl-10">
+                      <h1 className="text-2xl font-bold text-gray-900 mb-2">{product.name}</h1>
+                 <div className="text-xl font-semibold text-gray-700 mb-4">${product.price}</div>
+                      <p className="text-gray-700 mb-6">{product.description}</p>
+                 <div className="flex mb-4">
+                 <ItemCount 
+                       stock={product.stock}
+                       initial={1}
+                       onAdd={onAdd} />
+                  </div>
+                  </div>
             </div>
-        </>
+       </>
     )
 }
 export default ItemDetail;
