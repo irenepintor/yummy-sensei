670669444d090705599ad9d9c53a4eb80ac2bd56
Irene Pintor <103829940+irenepintor@users.noreply.github.com>
2024-06-10 08:33:23 -0300

    Update responsive design Item.jsx

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 
 const Item = ({ product }) => {
     return (
-        <Link to={`/item/${product.id}`} className="block bg-white p-4 mt-5 mb-4 rounded-lg shadow-md transition duration-300 hover:shadow-lg">
-            <img className="w-full h-48 object-cover rounded-lg mb-4" src={product.image} alt={product.name} />
-            <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
-            <p className="text-gray-700 mb-2">{product.description}</p>
-            <p className="text-blue-600 font-bold mb-4">${product.price}</p>
-        </Link>
+        <Link to={`/item/${product.id}`} className="block bg-white p-4 mt-5 mb-4 rounded-lg shadow-md transition duration-300 hover:shadow-lg md:flex md:flex-col md:h-full">
+    <img className="w-full h-48 object-cover rounded-lg mb-4 md:h-64" src={product.image} alt={product.name} />
+    <div className="flex flex-col flex-grow">
+        <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
+        <p className="text-gray-700 mb-2 flex-grow">{product.description}</p>
+        <p className="text-blue-600 font-bold mb-4">${product.price}</p>
+    </div>
+</Link>
     );
 };
 
